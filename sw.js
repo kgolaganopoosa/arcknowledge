f73c49190595ce3c52888bda85690540ccc4d4f8
kgolaganopoosa <kgolaganopeterpoo@gmail.com>
2026-06-29 16:16:30 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,37 @@
-const CACHE_NAME = 'arcknowledge-pwa-v1';
-const ASSETS_TO_CACHE = [
-  './',
-  './index.html',
-  './arc1.html',
-  './manifest.json',
-  './icon.svg'
+const CACHE_NAME = 'arcknowledge-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/lucide@latest',
+  'https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@400;500;600;700&display=swap'
 ];
 
+// Install Event
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS_TO_CACHE))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
   );
-  self.skipWaiting();
 });
 
+// Activate Event
 self.addEventListener('activate', event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
-      )
-    )
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
   );
-  self.clients.claim();
 });
 
+// Fetch Event (Cache First / Fallback to Network)
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(cachedResponse => {
